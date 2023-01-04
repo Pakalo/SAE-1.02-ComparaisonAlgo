9f58878fa1782cca3e7fdb024d0c9f1a9ec0b0a3
kyvinel <kyvinel@IUTLPINF16003>
2023-01-04 08:53:40 +0100

    calcul ok

--- SAE/main.cpp
+++ SAE/main.cpp
@@ -22,33 +22,46 @@ int main()
 		std::cerr << "Problème d'ouverture du fichier \"outputUTF8.csv\".\n";
 
 
-	out << "N; Aleat Sélect.; PresqueTri Sélect.; PresqueTriDeb Sélect.; PresqueTriDebFin Sélect.; PresqueTriFin Sélect\n";
+    out << "N; Aleat Sélect.; PresqueTri Sélect.; PresqueTriDeb Sélect.; PresqueTriDebFin Sélect.; PresqueTriFin Sélect\n";
 
-	for (int i = 3; i <= 200; i++)
-	{
-		
-		out << i << ";";
-		out << triSelection(initTabAleat(i)) << ";";
-		out << triSelection(initTabPresqueTri(i)) << ";";
-		out << triSelection(initTabPresqueTriDeb(i)) << ";";
-		out << triSelection(initTabPresqueTriDebFin(i)) << ";";
-		out << triSelection(initTabPresqueTriFin(i)) << "\n";
-	}
-	
+    for (int i = 3; i <= 20; i++)
+    {
 
-	out << "\n\n\n\nN²; Aleat Sélect.; PresqueTri Sélect.; PresqueTriDeb Sélect.; PresqueTriDebFin Sélect.; PresqueTriFin Sélect\n";
+        out << i << ";";
+        out << triSelection(initTabAleat(i)) << ";";
+        out << triSelection(initTabPresqueTri(i)) << ";";
+        out << triSelection(initTabPresqueTriDeb(i)) << ";";
+        out << triSelection(initTabPresqueTriDebFin(i)) << ";";
+        out << triSelection(initTabPresqueTriFin(i)) << "\n";
+    }
 
-	for (int i = 3; i <= 200; i++)
-	{
 
-		out << i*i << ";";
-		out << triSelection(initTabAleat(i-i)) << ";";
-		out << triSelection(initTabPresqueTri(i*i)) << ";";
-		out << triSelection(initTabPresqueTriDeb(i*i)) << ";";
-		out << triSelection(initTabPresqueTriDebFin(i*i)) << ";";
-		out << triSelection(initTabPresqueTriFin(i*i)) << "\n";
-	}
-	
+    out << "\n\n\n\nN²; Aleat Sélect.; PresqueTri Sélect 2; PresqueTriDeb Sélect2; PresqueTriDebFin Sélect2; PresqueTriFin Sélect2\n";
+
+    for (int i = 3; i <= 20; i++)
+    {
+
+        out << i * i << ";";
+        out << triSelection(initTabAleat(i * i)) << ";";
+        out << triSelection(initTabPresqueTri(i * i)) << ";";
+        out << triSelection(initTabPresqueTriDeb(i * i)) << ";";
+        out << triSelection(initTabPresqueTriDebFin(i * i)) << ";";
+        out << triSelection(initTabPresqueTriFin(i * i)) << "\n";
+    }
+
+
+    out << "\n\n\n\nN*ln(N); Aleat Sélect3; PresqueTri Sélect3; PresqueTriDeb Sélect3; PresqueTriDebFin Sélect3; PresqueTriFin Sélect3\n";
+
+    for (int i = 3; i <= 20; i++)
+    {
+
+        out << i * log(i) << ";";
+        out << triSelection(initTabAleat(i * log(i))) << ";";
+        out << triSelection(initTabPresqueTri(i * log(i))) << ";";
+        out << triSelection(initTabPresqueTriDeb(i * log(i))) << ";";
+        out << triSelection(initTabPresqueTriDebFin(i * log(i))) << ";";
+        out << triSelection(initTabPresqueTriFin(i * log(i))) << "\n";
+    }
 
 
 }
